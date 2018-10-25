338b9cda951518c61040690516e027dde0fbd074
Ed Vul <edwardvul@gmail.com>
2018-10-25 16:48:46 -0700

    eds R code

--- play.R
+++ play.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
-dat <- read_csv('fictionfabricsc.csv')
+dat <- read_csv('fictionfabrics%.csv')
+dat <- read_csv('fictionclothes%.csv')
 
 
 dat <- dat %>% 
@@ -8,19 +9,24 @@ dat <- dat %>%
   gather(key=word, value=count, -Year)
 
 dat %>% group_by(word) %>%
+  filter(count > 0) %>%
   mutate(total = sum(count)) %>%
   ungroup() %>%
   filter(total > 0) %>% 
-  mutate(count = log10(count+1)) %>%
+  mutate(count = log10(count)) %>%
   ggplot(aes(x=Year, y=count))+
   facet_wrap(~word) +
-  geom_line()
+  geom_point(size=0.1)+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle=90, hjust=-1, vjust=0),
+        strip.text = element_text(face = "bold"))
 
 dat %>% group_by(word) %>%
-  mutate(total = sum(count)) %>%
+  filter(count > 0) %>%
+  mutate(total = sum(count)) %>% 
   ungroup() %>%
   filter(total > 0) %>% 
-  mutate(count = log10(count+1)) %>%
+  mutate(count = log10(count)) %>%
   group_by(Year) %>%
   mutate(m = mean(count)) %>%
   ungroup() %>%
@@ -30,9 +36,39 @@ dat %>% group_by(word) %>%
   ungroup() %>%
   ggplot(aes(x=Year, y=zd.count))+
   facet_wrap(~word, ncol=10) +
-  geom_line()
+  geom_point(size=0.1)+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle=90, hjust=-1, vjust=0),
+        strip.text = element_text(face = "bold"))
 
-  
+acfs <- dat %>% group_by(word) %>%
+  mutate(count = ifelse(count <= 0, NA, count)) %>%
+  mutate(total = sum(count, na.rm = T)) %>% 
+  ungroup() %>%
+  filter(total > 0) %>% 
+  mutate(count = log10(count)) %>%
+  group_by(Year) %>%
+  mutate(m = mean(count, na.rm=T)) %>%
+  ungroup() %>%
+  mutate(d.count = count-m) %>%
+  group_by(word) %>%
+  mutate(zd.count = (d.count-mean(d.count, na.rm=T))/sd(d.count, na.rm = T)) %>%
+  # ungroup() %>% filter(word == 'denim') %>%
+  do(.$zd.count %>% 
+       ts() %>%
+    acf(na.action = na.pass) %>%
+      broom::tidy())
+
+
+acfs %>% ungroup() %>%
+  ggplot(aes(x=lag, y=acf))+
+  facet_wrap(~word, ncol=10) +
+  geom_line()+
+  theme_minimal()+
+  geom_hline(yintercept = 0, color='red')+
+  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0),
+        strip.text = element_text(face = "bold"))
+
 zdffts <- dat %>% group_by(word) %>%
   mutate(total = sum(count)) %>%
   ungroup() %>%
@@ -61,12 +97,9 @@ ffts <- dat %>%
               .[2:(length(.)/2)] %>%
               bind_cols(a=., n=1:length(.)))
 
-ffts %>% filter(n<=50) %>% 
+zdffts %>% filter(n<=50) %>% 
   mutate(a=log10(a)) %>%
   group_by(word) %>%
-  mutate(a = 0.5+a + 0.25*lag(a, n=1) + 0.25*lead(a, n=1)) %>%
-  mutate(a = 0.5+a + 0.25*lag(a, n=1) + 0.25*lead(a, n=1)) %>%
-  mutate(a = 0.5+a + 0.25*lag(a, n=1) + 0.25*lead(a, n=1)) %>%
-  mutate(a = 0.5+a + 0.25*lag(a, n=1) + 0.25*lead(a, n=1)) %>%
-  ggplot(aes(x=n, y=log10(a), color=word))+
-  geom_line()
+  ggplot(aes(x=n, y=(a)))+
+  geom_line()+
+  facet_wrap(~word, ncol=10)
